6190dbb314bc1331a03c9e1ee26d323abe676fd3
Luisfbejaranob <luis.bejarano@sofka.com.co>
2019-06-17 16:59:34 -0500

    Modelos Cliente y Proyecto

--- src/main/java/com/sofka/projectsmodule/models/ClientModel.java
+++ src/main/java/com/sofka/projectsmodule/models/ClientModel.java
@@ -1,5 +1,53 @@
 package com.sofka.projectsmodule.models;
 
-public class ClientModel {
+import java.util.ArrayList;
 
+import org.springframework.data.annotation.Id;
+
+public class ClientModel {
+	@Id
+	private int idClient;
+	private String nameClient;
+	private String productOwner;
+	private ArrayList<ProjectModel> projects;
+
+	
+	public ClientModel(int idClient, String nameClient, String productOwner, ArrayList<ProjectModel> projects) {
+		this.idClient = idClient;
+		this.nameClient = nameClient;
+		this.productOwner = productOwner;
+		this.projects = projects;
+	}
+
+	public int getidClient() {
+		return idClient;
+	}
+
+	public void setidClient(int idClient) {
+		this.idClient = idClient;
+	}
+
+	public String getNameClient() {
+		return nameClient;
+	}
+
+	public void setName(String name) {
+		this.nameClient = name;
+	}
+
+	public String getProductOwner() {
+		return productOwner;
+	}
+
+	public void setProductOwner(String productOwner) {
+		this.productOwner = productOwner;
+	}
+
+	public ArrayList<ProjectModel> getProjects() {
+		return projects;
+	}
+
+	public void setProjects(ArrayList<ProjectModel> projects) {
+		this.projects = projects;
+	}
 }
