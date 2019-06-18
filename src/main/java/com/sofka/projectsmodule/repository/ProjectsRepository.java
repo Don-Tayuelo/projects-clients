ac9fc25a2ed4ccb009d805660a796c3d5f6ee4b5
Don-Tayuelo <aljadis.tavera@sofka.com.co>
2019-06-18 09:37:11 -0500

    Solve implementations

--- src/main/java/com/sofka/projectsmodule/repository/ProjectsRepository.java
+++ src/main/java/com/sofka/projectsmodule/repository/ProjectsRepository.java
@@ -1,5 +1,14 @@
 package com.sofka.projectsmodule.repository;
 
-public interface ProjectsRepository {
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+
+import com.sofka.projectsmodule.models.ProjectModel;
+
+@Repository
+public interface ProjectsRepository extends MongoRepository<ProjectModel, String>{
+
+	public ProjectModel findByName(String nameProject);
+	
 }
--- src/main/java/com/sofka/projectsmodule/repository/impl/ProjectsRepositoryImpl.java
+++ src/main/java/com/sofka/projectsmodule/repository/impl/ProjectsRepositoryImpl.java
@@ -1,7 +1,17 @@
 package com.sofka.projectsmodule.repository.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.sofka.projectsmodule.models.ProjectModel;
 import com.sofka.projectsmodule.repository.ProjectsRepository;
 
-public class ProjectsRepositoryImpl implements ProjectsRepository {
-
+public class ProjectsRepositoryImpl {
+	
+	@Autowired
+	private ProjectsRepository projectsRepository;
+	
+	public ProjectModel findByName(String nameProject) {
+		
+		return projectsRepository.findByName(nameProject);
+	}
 }
